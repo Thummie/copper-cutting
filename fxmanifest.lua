0f7c4277738fb006d4b5d02d5695f1fa3438a88a
Thommie <53064581+Thummie@users.noreply.github.com>
2022-09-02 01:55:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,21 +2,12 @@ fx_version 'adamant'
 game 'gta5'
 
 author 'Thommie'
-description 'Money washing system'
+description 'Copper Cutting Script'
 
 shared_script 'config.lua'
-
-client_scripts {
-    'client/cl_main.lua',
-    'client/cl_dumpster.lua',
-    'client/cl_copper.lua',
-}
-
-server_scripts {
-    'server/sv_main.lua',
-    'server/sv_copper.lua',
-}
+client_script 'client/cl_copper.lua'
+server_script 'server/sv_copper.lua'
 
 dependencies {
-    'moon-target',
+    'qb-target',
 }
