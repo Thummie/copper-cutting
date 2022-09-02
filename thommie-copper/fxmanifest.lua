049ad23e4c1a976655059e8bcf244450dbf5313d
Thommie <53064581+Thummie@users.noreply.github.com>
2022-09-02 02:08:56 +0200

    Update fxmanifest.lua

--- thommie-copper/fxmanifest.lua
+++ thommie-copper/fxmanifest.lua
@@ -6,6 +6,10 @@ description 'Copper Cutting Script'
 
 lua54 'yes'
 
+escrow_ignore {
+  'config.lua',
+}
+
 shared_script 'config.lua'
 client_script 'client/cl_copper.lua'
 server_script 'server/sv_copper.lua'
