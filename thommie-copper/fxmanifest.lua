e68df9020838085937639599f296618195923ad9
Thommie <53064581+Thummie@users.noreply.github.com>
2022-09-02 02:06:38 +0200

    Update fxmanifest.lua

--- thommie-copper/fxmanifest.lua
+++ thommie-copper/fxmanifest.lua
@@ -4,6 +4,8 @@ game 'gta5'
 author 'Thommie'
 description 'Copper Cutting Script'
 
+lua54 'yes'
+
 shared_script 'config.lua'
 client_script 'client/cl_copper.lua'
 server_script 'server/sv_copper.lua'
