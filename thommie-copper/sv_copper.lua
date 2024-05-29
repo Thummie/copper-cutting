f0098117706490677d96b79ff274349690ffa181
Thommie <53064581+Thummie@users.noreply.github.com>
2024-05-29 17:11:09 +0200

    Reworked sv_copper.lua

--- thommie-copper/sv_copper.lua
+++ thommie-copper/sv_copper.lua
@@ -1,25 +1,20 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-CreateThread(function()
-    while true do
-        Wait(600000) -- Every 10 minutes check if copper is cut and reset at 20 min.
-        for k, v in pairs(Config.CutCopper) do
-            if Config.CutCopper[k].cut and Config.CutCopper[k].timeout ~= false then
-                if Config.CutCopper[k].timeout < os.time() then
-                    Config.CutCopper[k].cut = false
-                    Config.CutCopper[k].timeout = false
-                end
-            end
-        end
-        TriggerClientEvent('copper:updateCopper', -1, Config)
-    end
-end)
-
 -- Events
-RegisterNetEvent('copper:setState', function(location)
-    Config.CutCopper[location].cut = true
-    Config.CutCopper[location].timeout = (os.time() + 1200)
-    TriggerClientEvent('copper:cutState', -1, Config)
+RegisterNetEvent('copper:sync', function(entity, state)
+    Config.CutProps[entity] = state
+    TriggerClientEvent('copper:sync', -1, Config.CutProps)
+    if Config.Debug then
+        print('synced')
+    end
+
+    SetTimeout((Config.Copper.time * (60 * 1000)), function()
+        Config.CutProps[entity] = not state
+        TriggerClientEvent('copper:sync', -1, Config.CutProps)
+        if Config.Debug then
+            print('can use again')
+        end
+    end)
 end)
 
 RegisterNetEvent('copper:giveItem', function(item, amount)
